3c67e24fe66500cc904721927fb13f7d3ba5694e
Toma Gagne <toma.gagne.1@ulaval.ca>
2019-10-30 16:15:02 -0400

    Enlever des lignes de code inutiles

--- VirtuTuile/src/main/domain/room/Surface.java
+++ VirtuTuile/src/main/domain/room/Surface.java
@@ -20,26 +20,6 @@ public class Surface extends Polygon {
         super(x, y, number_of_summit);
     }
 
-    /*
-    //Constructeur surface rectangulaire
-    public Surface(String type, List<Point> points, boolean hole){
-        this.type = type;
-        this.points = points;
-        this.hole = hole;
-        this.selectionStatus = false;
-    }
-
-    //Constructeur surface irrégulière
-    public Surface(String type, int width, int height, List<Point> points, boolean hole){
-        this.type = type;
-        this.points = points;
-        this.width = width;
-        this.height = height;
-        this.selectionStatus = false;
-        this.hole = hole;
-    }
-     */
-
     public String getType() {
         return type;
     }
@@ -59,32 +39,6 @@ public class Surface extends Polygon {
     public int getHeight() {
         return this.height;
     }
-
-    /*
-    boolean contains(double x, double y){
-        return (xIsInsideSurfaceWidth(x) && yIsInsideSurfaceHeight(y));
-    }
-
-    boolean xIsInsideSurfaceWidth(double x) {
-        boolean xIsInside = true;
-        for (Point item : this.points) {
-            if ((x > (item.getX() + this.width)) && ((x < item.getX()))) {
-                xIsInside = false;
-            }
-        }
-        return xIsInside;
-    }
-
-    boolean yIsInsideSurfaceHeight(double y) {
-        boolean yIsInside = true;
-        for (Point item : this.points){
-            if ((y > item.getY() + this.height) && (y < item.getY())) {
-                yIsInside = false;
-            }
-        }
-        return yIsInside;
-    }
-     */
 
     void switchSelectionStatus() {
         this.selectionStatus =  !this.selectionStatus;
