66f02a6f577f5c9b600e19e30f0600ef64629822
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2019-11-06 14:10:25 -0500

    Modified UnitConverter

--- VirtuTuile/src/main/util/UnitConverter.java
+++ VirtuTuile/src/main/util/UnitConverter.java
@@ -1,5 +1,9 @@
 package util;
 
+import gui.MainWindow;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 /*import javafx.beans.binding.DoubleExpression;
 import javafx.util.Pair;*/
@@ -26,5 +30,22 @@ public class UnitConverter {
 
     public static double inchToMeter(double inch) {
         return inch * INCH_TO_METER;
+    }
+
+    public static double convertUnitToSelectedMode(double unit, MainWindow.MeasurementUnitMode mode) {
+        switch (mode) {
+            case METRIC:
+                return UnitConverter.pixelToMeter(unit);
+            case IMPERIAL:
+                return UnitConverter.pixelToInch(unit);
+        }
+        return unit;
+    }
+
+    public static Point2D convertPointToSelectedMode(Point2D point, MainWindow.MeasurementUnitMode mode) {
+        double xPos = UnitConverter.convertUnitToSelectedMode(point.getX(), mode);
+        double yPos = UnitConverter.convertUnitToSelectedMode(point.getY(), mode);
+        return new Point2D.Double(xPos, yPos);
+
     }
 }
--- VirtuTuile/src/test/util/UnitConverterTest.java
+++ VirtuTuile/src/test/util/UnitConverterTest.java
@@ -1,9 +1,10 @@
 package util;
 
-import org.junit.jupiter.api.AfterEach;
+import gui.MainWindow;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.awt.*;
+import java.awt.geom.Point2D;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +17,7 @@ class UnitConverterTest {
 
     @Test
     void pixelToMeter() {
-        assertEquals(0.0635, UnitConverter.pixelToMeter(240f), 0.001);
+        assertEquals(0.0635, UnitConverter.pixelToMeter(240d), 0.001);
     }
 
     @Test
@@ -27,5 +28,26 @@ class UnitConverterTest {
     @Test
     void inchToMeter() {
         assertEquals(1.9812, UnitConverter.inchToMeter(78d), 0.001);
+    }
+
+    @Test
+    void convertUnitToSelectedMode() {
+        assertEquals(1.8125, UnitConverter.convertUnitToSelectedMode(174d, MainWindow.MeasurementUnitMode.IMPERIAL), 0.001);
+        assertEquals(0.0635, UnitConverter.convertUnitToSelectedMode(240d, MainWindow.MeasurementUnitMode.METRIC), 0.001);
+    }
+
+    @Test
+    void convertPointToSelectedMode() {
+        Point2D pointToConvert = new Point2D.Double(167d, 249d);
+
+        Point2D pointInInches = new Point2D.Double(1.739583, 2.59375);
+        Point2D convertedPointInInches = UnitConverter.convertPointToSelectedMode(pointToConvert, MainWindow.MeasurementUnitMode.IMPERIAL);
+        assertEquals(pointInInches.getX(), convertedPointInInches.getX(), 0.001);
+        assertEquals(pointInInches.getY(), convertedPointInInches.getY(), 0.001);
+
+        Point2D pointInMeters = new Point2D.Double(0.0441854, 0.065881);
+        Point2D convertedPointInMeters = UnitConverter.convertPointToSelectedMode(pointToConvert, MainWindow.MeasurementUnitMode.METRIC);
+        assertEquals(pointInMeters.getX(), convertedPointInMeters.getX(), 0.001);
+        assertEquals(pointInMeters.getY(), convertedPointInMeters.getY(), 0.001);
     }
 }
